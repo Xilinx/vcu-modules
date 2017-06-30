44b1eef7b68db518db65201a2782365e46e2d9f0
Jeegar Patel <jeegarp@xilinx.com>
2017-06-30 15:20:48 -0700

    Add missing copyrights
    Signed-off-by: Kevin Grandemange <kevin.grandemange@allegrodvt.com>
    Signed-off-by: Jeegar Patel <jeegarp@xilinx.com>
    Signed-off-by: varunkumar allagadapa <varunkum@xilinx.com>

--- include/al_codec_mails.h
+++ include/al_codec_mails.h
@@ -1,3 +1,23 @@
+/*
+ * Copyright (C) 2016, Sebastien Alaiwan (sebastien.alaiwan@allegrodvt.com)
+ * Copyright (C) 2016, Kevin Grandemange (kevin.grandemange@allegrodvt.com)
+ * Copyright (C) 2016, Antoine Gruzelle (antoine.gruzelle@allegrodvt.com)
+ * Copyright (C) 2016, Allegro DVT (www.allegrodvt.com)
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
 #ifndef __AL_CODEC_MAILS__
 #define __AL_CODEC_MAILS__
 
