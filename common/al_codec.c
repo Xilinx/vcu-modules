37f980cebfcb4b0b94a9d97c17389ae7fe5f629d
Kevin Grandemange <kevin.grandemange@allegrodvt.com>
2017-11-03 10:59:58 +0100

    fix stop_mcu call
    Signed-off-by: Kevin Grandemange <kevin.grandemange@allegrodvt.com>

--- common/al_codec.c
+++ common/al_codec.c
@@ -348,7 +348,7 @@ int al5_codec_set_firmware(struct al5_codec_desc *codec, char *fw_file,
 	al5_group_unbind_user(&codec->users_group, &root);
 
 fail_init:
-	stop_mcu();
+	stop_mcu(codec);
 release_firmware:
 	release_firmware(fw);
 	release_firmware(bl_fw);
