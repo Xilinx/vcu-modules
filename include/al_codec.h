3c074a7daed99797fc673ae8cee73ff3c3210428
Kevin Grandemange <kevin.grandemange@allegrodvt.com>
2018-01-15 14:33:04 +0100

    Increase the maximum size the firmware for possible extension / plugins
    Signed-off-by: Kevin Grandemange <kevin.grandemange@allegrodvt.com>

--- include/al_codec.h
+++ include/al_codec.h
@@ -78,7 +78,7 @@
 /* MCU Cache */
 #define MCU_SUBALLOCATOR_SIZE           (1024 * 1024 * 16)       /* 16 MB */
 #define MCU_CACHE_OFFSET                0x80000000
-#define AL5_ICACHE_SIZE                 (1024 * 200)            /* 200 KB */
+#define AL5_ICACHE_SIZE                 (1024 * 600)            /* 600 KB (for possible extensions) */
 #define MCU_SRAM_SIZE                   0x8000                  /* 32 kB */
 
 struct al5_codec_desc {
