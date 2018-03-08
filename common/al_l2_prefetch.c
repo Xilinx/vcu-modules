ccc751ce1353150e543ba60a3d3c86d5a7852ea2
Jeegar Patel <jeegar.patel@xilinx.com>
2018-03-08 15:48:52 -0800

    Add missing files from previous commit to fix compilation issue
    These two files were missed in previous commit, But makefile has
    entries of them so build was broken.
    
    Signed-off-by: Jeegar Patel <jeegar.patel@xilinx.com>

---
+++ common/al_l2_prefetch.c
@@ -0,0 +1,13 @@
+#include <linux/types.h>
+
+u32 get_l2_color_bitdepth(void *parent)
+{
+	(void)parent;
+	return -1;
+}
+
+u32 get_l2_size_in_bits(void *parent)
+{
+	(void)parent;
+	return -1;
+}
---
+++ common/xil_l2_prefetch.c
@@ -0,0 +1,25 @@
+#include <linux/types.h>
+
+#include <soc/xilinx/xlnx_vcu.h>
+
+#define MEMORY_WORD_SIZE_IN_BITS 32
+
+u32 get_l2_color_bitdepth(void *parent)
+{
+	struct xvcu_device *xvcu = (struct xvcu_device *)parent;
+
+	return xvcu_get_color_depth(xvcu) == 0 ? 8 : 10;
+}
+
+static u32 get_memory_words_in_bits(struct xvcu_device *xvcu)
+{
+	return get_l2_color_bitdepth(xvcu) * MEMORY_WORD_SIZE_IN_BITS;
+}
+
+u32 get_l2_size_in_bits(void *parent)
+{
+	struct xvcu_device *xvcu = (struct xvcu_device *)parent;
+
+	return xvcu_get_memory_depth(xvcu) * get_memory_words_in_bits(xvcu);
+}
+
